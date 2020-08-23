1e04b7a2d09e4e5b5a6855bbae685e9ebb3a63c6
charanbhavaraju2001 <68843337+charanbhavaraju2001@users.noreply.github.com>
2020-08-23 12:45:16 +0530

    Create studentgrade.c

---
+++ studentgrade.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+int main()
+{
+    int marks[5],i,sum=0;
+    float avg;
+    char grade;
+    printf("Enter your marks in 5 Subjects (Math,Physics,Chemistry,Programming,English)\n");
+    for(i=0;i<5;i++)
+    {
+        scanf("%d",&marks[i]);
+        sum = sum + marks[i];
+    }
+    avg = sum / 5 ;
+    printf("You scored %f percentage\n",avg);
+    
+   if(avg >= 90)
+    {
+        grade = 'A';
+    }
+    else if(avg >= 80 && avg < 90)
+    {
+        grade = 'B';
+    }
+    else if(avg >= 70 && avg < 80)
+    {
+        grade = 'C';
+    }
+    else if(avg >= 60 && avg < 70)
+    {
+        grade = 'D';
+    }
+    else if(avg >= 50 && avg < 60)
+    {
+        grade = 'E';
+    }
+    else 
+    {
+        grade = 'F';
+    }
+                    
+    printf("Your grade is %c \n", grade);
+    
+    
+    
+}
